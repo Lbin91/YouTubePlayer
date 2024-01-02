91fe55752243cceffabda65af35b46914579e341
Bongjin Lee <bongjin.lee.dev@gmail.com>
2024-01-02 12:01:21 +0900

    remove test

--- Package.swift
+++ Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "YouTubePlayer",
             dependencies: []),
-        .testTarget(
-            name: "YouTubePlayerTests",
-            dependencies: ["YouTubePlayer"]),
     ]
 )
