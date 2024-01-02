b17e6e7b9d8c05d2de81d03cdf0a2c861f76222e
Bongjin Lee <bongjin.lee.dev@gmail.com>
2024-01-02 12:16:32 +0900

    Revert "remove test"
    This reverts commit 91fe55752243cceffabda65af35b46914579e341.

--- Package.swift
+++ Package.swift
@@ -21,5 +21,8 @@ let package = Package(
         .target(
             name: "YouTubePlayer",
             dependencies: []),
+        .testTarget(
+            name: "YouTubePlayerTests",
+            dependencies: ["YouTubePlayer"]),
     ]
 )
